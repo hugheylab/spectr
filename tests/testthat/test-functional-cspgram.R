5bc12e4c08504f73c34cf4f8adf5dc13b7f2ca71
Josh Schoenbachler <josh.schoenbachler@gmail.com>
2020-05-29 09:56:18 -0500

    Testing updates

--- tests/testthat/test-functional-cspgram.R
+++ tests/testthat/test-functional-cspgram.R
@@ -10,18 +10,15 @@ test_that('CSP Greedy', {
   tt = seq(0, 24 * 3, deltat)
   x = 3 * sin(tt / tau * 2 * pi) + rnorm(length(tt))
   cspGr = cspgram(x, deltat,periodRange = c(20,30), method = 'greedy', dopar = FALSE)
-  cspGr = cspgram(x, deltat, method = 'greedy', dopar = FALSE)
-  cspGrPeak = cspGr[which.min(log_pval)]
+  write.csv(cspGr, file = "cspGrGen.csv")
+
   cspGrExp = fread('cspGr.csv')
+  cspGrExp[,V1:=NULL]
+
   allEqTest1 = all.equal(cspGr,cspGrExp,check.attributes=FALSE)
-  print(allEqTest1)
+  write(allEqTest1, file = "allEqTest1.txt")
   expect_true(all.equal(cspGr,cspGrExp,check.attributes=FALSE))
-  cspGrPeakExp = data.table(period = 24.4,
-  chisq = 635.4408,
-  df = 243,
-  log_pval = (-83.23465),
-  pval = (7.106419 * 10 ^ (-37)))
-  expect_true(all.equal(cspGrPeak,cspGrPeakExp,check.attributes=FALSE))
+
 })
 
 test_that('CSP Standard', {
@@ -31,14 +28,15 @@ test_that('CSP Standard', {
   tau = 25
   tt = seq(0, 24 * 3, deltat)
   x = 3 * sin(tt / tau * 2 * pi) + rnorm(length(tt))
-  cspSt = cspgram(x, deltat, method = 'standard', dopar = FALSE)
-  cspStPeak = cspSt[which.min(log_pval)]
-  cspStPeakExp = data.table(period = 24,
-  chisq = 616.0034,
-  df = 239,
-  log_pval = (-79.13604),
-  pval = (4.282085 * 10 ^ (-35)))
-  expect_true(all.equal(cspStPeak,cspStPeakExp,check.attributes=FALSE,tolerance = 0.001))
+  cspSt = cspgram(x, deltat,periodRange = c(20,30), method = 'standard', dopar = FALSE)
+  write.csv(cspSt, file = "cspStGen.csv")
+
+  cspStExp = fread('cspSt.csv')
+  cspStExp[,V1:=NULL]
+
+  allEqTest2 = all.equal(cspSt,cspStExp,check.attributes=FALSE,tolerance = 0.001)
+  write(allEqTest2, file = "allEqTest2.txt")
+  expect_true(all.equal(cspSt,cspStExp,check.attributes=FALSE,tolerance = 0.001))
 })
 
 test_that('CSP Conservative', {
@@ -48,12 +46,13 @@ test_that('CSP Conservative', {
   tau = 25
   tt = seq(0, 24 * 3, deltat)
   x = 3 * sin(tt / tau * 2 * pi) + rnorm(length(tt))
-  cspCo = cspgram(x, deltat, method = 'conservative', dopar = FALSE)
-  cspCoPeak = cspCo[which.min(log_pval)]
-  cspCoPeakExp = data.table(period = 25.5,
-  chisq = 468.7745,
-  df = 254,
-  log_pval = (-32.75673),
-  pval = (5.942029 * 10 ^ (-15)))
-  expect_true(all.equal(cspCoPeak,cspCoPeakExp,check.attributes=FALSE,tolerance = 0.001))
+  cspCo = cspgram(x, deltat,periodRange = c(20,30), method = 'conservative', dopar = FALSE)
+  write.csv(cspCo, file = "cspCoGen.csv")
+
+  cspCoExp = fread('cspCo.csv')
+  cspCoExp[,V1:=NULL]
+
+  allEqTest3 = all.equal(cspCo,cspCoExp,check.attributes=FALSE,tolerance = 0.001)
+  write(allEqTest3, file = "allEqTest3.txt")
+  expect_true(all.equal(cspCo,cspCoExp,check.attributes=FALSE,tolerance = 0.001))
 })
