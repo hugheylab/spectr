58d5f5d9c41209b77c2f9e9903858ceeefa7ac3c
Josh Schoenbachler <josh.schoenbachler@gmail.com>
2020-06-01 12:28:47 -0500

    Add Unit tests without errors for throwing errors

---
+++ tests/testthat/test-unit-cspgram.R
@@ -0,0 +1,34 @@
+context('cspgram Unit Tests')
+
+
+
+test_that('CSP Greedy', {
+  library(data.table)
+  set.seed(1789)
+  deltat = 0.1
+  tau = 25
+  tt = seq(0, 24 * 3, deltat)
+  x = 3 * sin(tt / tau * 2 * pi) + rnorm(length(tt))
+  cspGr = cspgram(x, deltat,periodRange = c(20,30), method = 'greedy', dopar = FALSE)
+
+})
+
+test_that('CSP Standard', {
+  library(data.table)
+  set.seed(1789)
+  deltat = 0.1
+  tau = 25
+  tt = seq(0, 24 * 3, deltat)
+  x = 3 * sin(tt / tau * 2 * pi) + rnorm(length(tt))
+  cspSt = cspgram(x, deltat,periodRange = c(20,30), method = 'standard', dopar = FALSE)
+})
+
+test_that('CSP Conservative', {
+  library(data.table)
+  set.seed(1789)
+  deltat = 0.1
+  tau = 25
+  tt = seq(0, 24 * 3, deltat)
+  x = 3 * sin(tt / tau * 2 * pi) + rnorm(length(tt))
+  cspCo = cspgram(x, deltat,periodRange = c(20,30), method = 'conservative', dopar = FALSE)
+})
