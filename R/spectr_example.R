7e6a7523f3906714a0a9df3995184f5755988581
Josh Schoenbachler <josh.schoenbachler@gmail.com>
2020-06-02 15:31:54 -0500

    Add functional tests for spectr

--- R/spectr_example.R
+++ R/spectr_example.R
@@ -6,5 +6,17 @@ tau = 25
 tt = seq(0, 24 * 3, deltat)
 x = 3 * sin(tt / tau * 2 * pi) + rnorm(length(tt))
 
-spec = spectr(x, deltat)
-specPeak = spec[which.min(log_pval)]
+spec_Greedy = spectr(x, deltat,periodRange = c(20,30), method = "greedy_chisq")
+write.csv(spec_Greedy, file = "spec_Greedy")
+
+spec_Cons = spectr(x, deltat,periodRange = c(20,30), method = "conservative_chisq")
+write.csv(spec_Cons, file = "spec_Cons")
+
+spec_Stand = spectr(x, deltat,periodRange = c(20,30), method = "standard_chisq")
+write.csv(spec_Stand, file = "spec_Stand")
+
+spec_Lombs = spectr(x, deltat,periodRange = c(20,30), method = "lombscargle")
+write.csv(spec_Lombs, file = "spec_Lombs")
+
+spec_Fft = spectr(x, deltat,periodRange = c(20,30), method = "fft")
+write.csv(spec_Fft, file = "spec_Fft")
