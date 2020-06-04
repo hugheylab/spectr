2d5abaa3d6c5f2ad1fb1f24f33d45e5768999efc
Josh Schoenbachler <josh.schoenbachler@gmail.com>
2020-06-04 11:27:20 -0500

    Add FFT and Lomb Scargle functional tests for individual Methods

---
+++ tests/testthat/test-functional-lombscargle.R
@@ -0,0 +1,21 @@
+context('Lomb Scargle Functional Tests')
+
+
+
+test_that('Lomb Scargle', {
+  library(data.table)
+  set.seed(1789)
+  deltat = 0.1
+  tau = 25
+  tt = seq(0, 24 * 3, deltat)
+  x = 3 * sin(tt / tau * 2 * pi) + rnorm(length(tt))
+  lombscargle = lspgram(x, deltat,periodRange = c(20,30))
+  write.csv(lombscargle, file = "lombSGen.csv")
+
+  lombscargleExp = fread('spec_Lombs.csv')
+  lombscargleExp[,V1:=NULL]
+
+  lombSAllEqTest = all.equal(lombscargle,lombscargleExp,check.attributes=FALSE,tolerance = 0.001)
+  write(lombSAllEqTest, file = "lombSAllEqTest.txt")
+  expect_true(all.equal(lombscargle,lombscargleExp,check.attributes=FALSE,tolerance = 0.001))
+})
