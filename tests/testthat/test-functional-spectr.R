1f8e71a255b63fa71335fc58d0a0915146dbb495
Josh Schoenbachler <josh.schoenbachler@gmail.com>
2020-06-08 12:35:39 -0500

    Update snake case Again

--- tests/testthat/test-functional-spectr.R
+++ tests/testthat/test-functional-spectr.R
@@ -29,7 +29,7 @@ test_that('spectr CSP Conservative', {
   x = 3 * sin(tt / tau * 2 * pi) + rnorm(length(tt))
 
   conservative_chisq = spectr(x, deltat, periodRange = c(20,30), method = 'conservative_chisq')
-  fwrite(conservative_chisq, file = "spec_ConsGen.csv")
+  fwrite(conservative_chisq, file = "spec_Cons_Gen.csv")
 
   conservative_chisqExp = fread('spec_Cons.csv')
 
@@ -47,7 +47,7 @@ test_that('spectr CSP Standard', {
   x = 3 * sin(tt / tau * 2 * pi) + rnorm(length(tt))
 
   standard_chisq = spectr(x, deltat, periodRange = c(20,30), method = 'standard_chisq')
-  fwrite(standard_chisq, file = "spec_StandGen.csv")
+  fwrite(standard_chisq, file = "spec_Stand_Gen.csv")
 
   standard_chisqExp = fread('spec_Stand.csv')
 
@@ -65,7 +65,7 @@ test_that('spectr Lomb Scargle', {
   x = 3 * sin(tt / tau * 2 * pi) + rnorm(length(tt))
 
   lombscargle = spectr(x, deltat, periodRange = c(20,30), method = 'lombscargle')
-  fwrite(lombscargle, file = "spec_LombsGen.csv")
+  fwrite(lombscargle, file = "spec_Lombs_Gen.csv")
 
   lombscargleExp = fread('spec_Lombs.csv')
 
@@ -83,7 +83,7 @@ test_that('spectr FFT', {
   x = 3 * sin(tt / tau * 2 * pi) + rnorm(length(tt))
 
   fft = spectr(x, deltat,periodRange = c(20,30), method = 'fft')
-  fwrite(fft, file = "spec_FftGen.csv")
+  fwrite(fft, file = "spec_Fft_Gen.csv")
 
   fftExp = fread('spec_Fft.csv')
 
