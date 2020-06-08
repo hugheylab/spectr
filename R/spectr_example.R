36b317a7dbab697d93a67a93e7f6467793512e0d
Josh Schoenbachler <josh.schoenbachler@gmail.com>
2020-06-08 10:39:58 -0500

    Revert to Master Branch

--- R/spectr_example.R
+++ R/spectr_example.R
@@ -6,17 +6,5 @@ tau = 25
 tt = seq(0, 24 * 3, deltat)
 x = 3 * sin(tt / tau * 2 * pi) + rnorm(length(tt))
 
-spec_Greedy = spectr(x, deltat,periodRange = c(20,30), method = "greedy_chisq")
-fwrite(spec_Greedy, file = "spec_Greedy.csv")
-
-spec_Cons = spectr(x, deltat,periodRange = c(20,30), method = "conservative_chisq")
-fwrite(spec_Cons, file = "spec_Cons.csv")
-
-spec_Stand = spectr(x, deltat,periodRange = c(20,30), method = "standard_chisq")
-fwrite(spec_Stand, file = "spec_Stand.csv")
-
-spec_Lombs = spectr(x, deltat,periodRange = c(20,30), method = "lombscargle")
-fwrite(spec_Lombs, file = "spec_Lombs.csv")
-
-spec_Fft = spectr(x, deltat,periodRange = c(20,30), method = "fft")
-fwrite(spec_Fft, file = "spec_Fft.csv")
+spec = spectr(x, deltat)
+specPeak = spec[which.min(log_pval)]
