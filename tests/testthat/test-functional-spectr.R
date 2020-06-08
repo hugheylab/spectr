93032e962987edfb32d49f3dd5619e316b928c43
Josh Schoenbachler <josh.schoenbachler@gmail.com>
2020-06-08 12:35:15 -0500

    Update for snake case

--- tests/testthat/test-functional-spectr.R
+++ tests/testthat/test-functional-spectr.R
@@ -10,12 +10,12 @@ test_that('spectr CSP Greedy', {
   x = 3 * sin(tt / tau * 2 * pi) + rnorm(length(tt))
 
   greedy_chisq = spectr(x, deltat, periodRange = c(20,30), method = 'greedy_chisq')
-  fwrite(greedy_chisq, file = "spec_GreedyGen.csv")
+  fwrite(greedy_chisq, file = "spec_Greedy_Gen.csv")
 
   greedy_chisqExp = fread('spec_Greedy.csv')
 
   spectrAllEqTest1 = all.equal(greedy_chisq, greedy_chisqExp, check.attributes = FALSE)
-  write(spectrAllEqTest1, file = "spectrAllEqTest1.txt")
+  write(spectrAllEqTest1, file = "spectr_All_Eq_Test1.txt")
   expect_true(all.equal(greedy_chisq, greedy_chisqExp, check.attributes = FALSE))
 
 })
@@ -34,7 +34,7 @@ test_that('spectr CSP Conservative', {
   conservative_chisqExp = fread('spec_Cons.csv')
 
   spectrAllEqTest2 = all.equal(conservative_chisq, conservative_chisqExp, check.attributes = FALSE, tolerance = 0.001)
-  write(spectrAllEqTest2, file = "spectrAllEqTest2.txt")
+  write(spectrAllEqTest2, file = "spectr_All_Eq_Test2.txt")
   expect_true(all.equal(conservative_chisq, conservative_chisqExp, check.attributes = FALSE, tolerance = 0.001))
 })
 
@@ -52,7 +52,7 @@ test_that('spectr CSP Standard', {
   standard_chisqExp = fread('spec_Stand.csv')
 
   spectrAllEqTest3 = all.equal(standard_chisq, standard_chisqExp, check.attributes = FALSE, tolerance = 0.001)
-  write(spectrAllEqTest3, file = "spectrAllEqTest3.txt")
+  write(spectrAllEqTest3, file = "spectr_All_Eq_Test3.txt")
   expect_true(all.equal(standard_chisq, standard_chisqExp, check.attributes = FALSE, tolerance = 0.001))
 })
 
@@ -70,7 +70,7 @@ test_that('spectr Lomb Scargle', {
   lombscargleExp = fread('spec_Lombs.csv')
 
   spectrAllEqTest4 = all.equal(lombscargle, lombscargleExp, check.attributes = FALSE, tolerance = 0.001)
-  write(spectrAllEqTest4, file = "spectrAllEqTest4.txt")
+  write(spectrAllEqTest4, file = "spectr_All_Eq_Test4.txt")
   expect_true(all.equal(lombscargle, lombscargleExp, check.attributes = FALSE, tolerance = 0.001))
 })
 
@@ -88,6 +88,6 @@ test_that('spectr FFT', {
   fftExp = fread('spec_Fft.csv')
 
   spectrAllEqTest5 = all.equal(fft, fftExp, check.attributes = FALSE, tolerance = 0.001)
-  write(spectrAllEqTest5, file = "spectrAllEqTest5.txt")
+  write(spectrAllEqTest5, file = "spectr_All_Eq_Test5.txt")
   expect_true(all.equal(fft, fftExp, check.attributes = FALSE, tolerance = 0.001))
 })
