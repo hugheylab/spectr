9b033673c592ad95b178faf3a657b7d9b90163dc
Josh Schoenbachler <josh.schoenbachler@gmail.com>
2020-06-10 14:45:49 -0500

    Force Expect Error

--- tests/testthat/test-error-check-input.R
+++ tests/testthat/test-error-check-input.R
@@ -3,18 +3,18 @@ context('check_input Error Tests')
 
 test_that('checkX', {
     
-    expect_silent(checkX(x = c('aaa',2)))
+    expect_error(checkX(x = c('aaa',2)))
 
 })
 
 test_that('checkSingleNum', {
     
-    expect_silent(checkSingleNum(z = c('aaa',2), lower = 3, inclusive = TRUE))
+    expect_error(checkSingleNum(z = c('aaa',2), lower = 3, inclusive = TRUE))
 
 })
 
 test_that('checkTime', {
     
-    expect_silent(checkTime(x = c('aaa',2)))
+    expect_error(checkTime(x = c('aaa',2)))
 
 })
