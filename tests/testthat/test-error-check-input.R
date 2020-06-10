e57673cc58e99b94bede169d20920b38c91fd1ee
Josh Schoenbachler <josh.schoenbachler@gmail.com>
2020-06-10 14:28:08 -0500

    Add Additional Checks

--- tests/testthat/test-error-check-input.R
+++ tests/testthat/test-error-check-input.R
@@ -1,8 +1,20 @@
 context('check_input Error Tests')
 
 
-test_that('checkx', {
+test_that('checkX', {
     
     expect_silent(checkX(x = c('aaa',2)))
+
+})
+
+test_that('checkSingleNum', {
+    
+    expect_silent(checkSingleNum(z = c('aaa',2), lower = 3, inclusive = TRUE))
+
+})
+
+test_that('checkTime', {
+    
+    expect_silent(checkTime(x = c('aaa',2)))
 
 })
